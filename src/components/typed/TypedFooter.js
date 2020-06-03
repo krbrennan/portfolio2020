9908a7208152945f7bd1dec566a9b04cb78c9d4f
krbrennan <krb.brennan@gmail.com>
2020-06-03 14:44:05 -0400

    footer is responsive and styled. working on main homepage text and nav responsiveness

--- src/components/typed/TypedFooter.js
+++ src/components/typed/TypedFooter.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import Typed from 'react-typed';
+import { Link } from 'react-router-dom';
 
 // style
 import './typed-footer.scss';
 
-const TypedFooter = () => {
-    return(
-        <div className='home-footer'>
+// const TypedFooter = () => {
+//     return(
+//         <div className='home-footer'>
+//             <Typed 
+//                 strings={["Hi there...","Check out my projects..."]}
+//                 typeSpeed={200}
+//                 backSpeed={100}
+//                 className='typed-words'
+//                 loop={false}
+//                 onStop={console.log('anus')}
+//             />
+//             <a className='projects-btn-a' target='_blank' href='/projects'><button className='projects-btn'>explore</button></a>
+//         </div>
+//     )
+// }
+
+
+class TypedFooter extends React.Component {
+    state = {
+        displayBtn: false
+    }
+
+    displayProjectBtn = () => {
+        this.setState({displayBtn: true})
+    }
+
+    render = () => {
+        setTimeout(this.displayProjectBtn, 12000);
+        return(
+            <div className='home-footer'>
             <Typed 
                 strings={["Hi there...","Check out my projects..."]}
                 typeSpeed={200}
                 backSpeed={100}
                 className='typed-words'
-                loop={true}
+                loop={false}
+                onStop={console.log('anus')}
             />
-            <a className='projects-btn-a' target='_blank' href='/projects'><h3 className='projects-btn'>explore</h3></a>
+            {/* <a className='project-btn-a' target='_blank' href='/projects'><button className='projects-btn'>explore</button></a> */}
+            { this.state.displayBtn ? <Link to="/projects"><button className="projects-btn">explore</button></Link> : <Link to="/projects"><button className='hidden'></button></Link>}
         </div>
-    )
+        )
+    }
 }
 
 export default TypedFooter;
