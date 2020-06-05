764668b01c9213913f472364306fa85dc439e325
krbrennan <krb.brennan@gmail.com>
2020-06-05 14:31:56 -0400

    project cards correctly rendering images. formatting cards and adding real content to them

--- src/components/projects/ProjectListItem.js
+++ src/components/projects/ProjectListItem.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
+import './project_assets/projects.json';
+
 // styles
 import './projectListItem.scss';
 
+require('./project_assets/weatherEssentials.jpeg');
+
 const ProjectListItem = (props) => {
-console.log(props)
+    // let image = ;
+    // console.log({require(props.project.image)})
     return(
         <li className='project-list-item-li'>
             <div className='project-list-item-div'>
                 <div className='top-div'>
-                    <p>Image goes here</p>
+                    <img className='image' src={ require(`${props.project.image}`) }></img>
                 </div>
                 <div className='bottom-div'>
                     <h3 className='project-list-item-h3'>{props.project.name}</h3>
                     <p className='project-icons'>{props.project.icons}</p>
                     <p className='blurb'>{props.project.blurb}</p>
-                    <p className='project-api'>{props.project.api}</p>
+                    <p className='project-api'>API used: {props.project.api ? props.project.api : "none"}</p>
                 </div>
             </div>
         </li>
