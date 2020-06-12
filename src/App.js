ab374d922eda09029c57ca419193e159ca190646
krbrennan <krb.brennan@gmail.com>
2020-06-12 18:22:22 -0400

    attempting route change to fix gh-pages rendering issue

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
   return(
   <Router>
     <Switch>
+      <Route exact path='/' component={ Home } />
       <Route exact path='/about' component={ About } />
       <Route exact path='/projects' component={ Projects } />
       <Route exact path='/connect' component={ Connect } />
-      <Route exact path='/' component={ Home } />
     </Switch>
   </Router>
   )
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
